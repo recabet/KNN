f28adea64df77cebaaacc347bb2bfd41d7ca7f00
recabet <rajab.iskenderli@gmail.com>
2024-07-21 21:06:40 +0400

    minor changes

--- model.py
+++ model.py
@@ -1,24 +1,7 @@
 import numpy as np
 
-def euclidean_distance (a, b):
-	a = np.array(a)
-	b = np.array(b)
-	return np.linalg.norm(a - b)
 
-
-def manhattan_distance (a, b):
-	a = np.array(a)
-	b = np.array(b)
-	return np.sum(np.abs(a - b))
-
-
-def hamming_distance (a, b):
-	if len(a) != len(b):
-		raise ValueError("Sequences must be of the same length")
-	return sum(el1 != el2 for el1, el2 in zip(a, b))
-
-
-class KNN:
+class my_KNN:
 	def __init__ (self, k=5, indexing="euclidean"):
 		if k % 2 == 0:
 			raise ValueError("K must be an odd number")
@@ -36,18 +19,33 @@ class KNN:
 		predictions = [self.__predict(x) for x in X]
 		return np.array(predictions)
 	
+	@staticmethod
+	def euclidean_distance (a, b):
+		a = np.array(a)
+		b = np.array(b)
+		return np.linalg.norm(a - b)
+	@staticmethod
+	def manhattan_distance (a, b):
+		a = np.array(a)
+		b = np.array(b)
+		return np.sum(np.abs(a - b))
+	@staticmethod
+	def hamming_distance (a, b):
+		if len(a) != len(b):
+			raise ValueError("Sequences must be of the same length")
+		return sum(el1 != el2 for el1, el2 in zip(a, b))
+	
 	def __predict (self, x):
 		match self.indexing:
 			case "euclidean":
-				distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+				distances = [my_KNN.euclidean_distance(x, x_train) for x_train in self.X_train]
 			case "manhattan":
-				distances = [manhattan_distance(x, x_train) for x_train in self.X_train]
+				distances = [my_KNN.manhattan_distance(x, x_train) for x_train in self.X_train]
 			case "hamming":
-				distances = [hamming_distance(x, x_train) for x_train in self.X_train]
+				distances = [my_KNN.hamming_distance(x, x_train) for x_train in self.X_train]
 			case _:
 				raise ValueError("Unknown indexing method")
-		
-		
+
 		k_indices = np.argsort(distances)[:self.k]
 		k_nearest_labels = [self.y_train[i] for i in k_indices]
 		most_common = np.bincount(k_nearest_labels).argmax()
