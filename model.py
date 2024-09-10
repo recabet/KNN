1ec98107921c6e95673eef60286632d0a354b830
recabet <rajab.iskenderli@gmail.com>
2024-09-10 19:26:40 +0400

    Changed predict and added optimal K

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 import numpy as np
 from typing import List, Union
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 
 class my_KNN:
@@ -43,93 +45,71 @@ class my_KNN:
         self.X_train = np.array(X)
         self.y_train = np.array(y)
     
-    def predict (self, X: Union[np.ndarray, List[List[float]]]) -> np.ndarray:
-        """
-        Predict the labels for the input data.
-
-        :param X: Input data for prediction.
-        :type X: Union[np.ndarray, List[List[float]]]
-
-        :return: Predicted labels.
-        :rtype: np.ndarray
-        """
-        X = np.array(X)
-        predictions = [self.__predict(x) for x in X]
-        return np.array(predictions)
-    
-    @staticmethod
-    def euclidean_distance (a: np.ndarray, b: np.ndarray) -> float:
-        """
-        Compute the Euclidean distance between two points.
-
-        :param a: First point.
-        :type a: np.ndarray
-        :param b: Second point.
-        :type b: np.ndarray
-
-        :return: Euclidean distance.
-        :rtype: float
-        """
-        return float(np.linalg.norm(a - b))
-    
-    @staticmethod
-    def manhattan_distance (a: np.ndarray, b: np.ndarray) -> float:
-        """
-        Compute the Manhattan distance between two points.
-
-        :param a: First point.
-        :type a: np.ndarray
-        :param b: Second point.
-        :type b: np.ndarray
-
-        :return: Manhattan distance.
-        :rtype: float
-        """
-        return np.sum(np.abs(a - b))
-    
-    @staticmethod
-    def hamming_distance (a: np.ndarray, b: np.ndarray) -> int:
-        """
-        Compute the Hamming distance between two sequences.
-
-        :param a: First sequence.
-        :type a: np.ndarray
-        :param b: Second sequence.
-        :type b: np.ndarray
-
-        :return: Hamming distance.
-        :rtype: int
-
-        :raises ValueError: If the sequences have different lengths.
-        """
-        if len(a) != len(b):
-            raise ValueError("Sequences must be of the same length")
-        return np.sum(a != b)
-    
-    def __predict (self, x: np.ndarray) -> int:
+    def predict (self, X: Union[np.ndarray, List[float]]) -> int:
         """
         Predict the label for a single data point.
 
-        :param x: Data point to classify.
-        :type x: np.ndarray
+        :param X: Data point to classify.
+        :type X: Union[np.ndarray, List[float]]
 
         :return: Predicted label.
         :rtype: int
 
-        :raises ValueError: If an unknown indexing method is specified.
+        :raises ValueError: If an unknown indexing method is specified or distance computation is invalid.
         """
+        X = np.array(X)
+        
         if self.indexing == "euclidean":
-            distances = np.linalg.norm(self.X_train - x, axis=1)
+            distances = np.linalg.norm(self.X_train - X, axis=1)
         elif self.indexing == "manhattan":
-            distances = np.sum(np.abs(self.X_train - x), axis=1)
+            distances = np.sum(np.abs(self.X_train - X), axis=1)
         elif self.indexing == "hamming":
-            if x.shape[0] != self.X_train.shape[1]:
+            if X.shape[0] != self.X_train.shape[1]:
                 raise ValueError("Feature length of input and training data must be the same for Hamming distance")
-            distances = np.sum(self.X_train != x, axis=1)
+            distances = np.sum(self.X_train != X, axis=1)
         else:
             raise ValueError("Unknown indexing method")
         
         k_indices = np.argsort(distances)[:self.k]
         k_nearest_labels = self.y_train[k_indices]
         most_common = np.bincount(k_nearest_labels).argmax()
+        
         return int(most_common)
+    
+    def find_optimal_k (self, X: Union[np.ndarray, List[List[float]]], y: Union[np.ndarray, List[int]],
+                        k_values: List[int] = None, test_size: float = 0.2) -> int:
+        """
+        Find the optimal value of k using a range of k values.
+
+        :param X: Training data.
+        :type X: Union[np.ndarray, List[List[float]]]
+        :param y: Labels for the training data.
+        :type y: Union[np.ndarray, List[int]]
+        :param k_values: List of k values to try. If None, a default range from 1 to 20 is used.
+        :type k_values: List[int]
+        :param test_size: Proportion of data to use for testing.
+        :type test_size: float
+
+        :return: The optimal value of k based on accuracy.
+        :rtype: int
+        """
+        if k_values is None:
+            k_values = list(range(1, 21, 2))
+        
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=test_size, random_state=42)
+        
+        accuracies = []
+        
+        for k in k_values:
+            self.k = k
+            self.fit(X_train, y_train)
+            predictions = [self.predict(x) for x in X_val]
+            accuracy = accuracy_score(y_val, predictions)
+            accuracies.append((k, accuracy))
+        
+        best_k = max(accuracies, key=lambda item: item[1])[0]
+        
+        print(f"Optimal k found: {best_k} with accuracy: {max(accuracies, key=lambda item: item[1])[1]:.2f}")
+        self.k = best_k
+        
+        return best_k
